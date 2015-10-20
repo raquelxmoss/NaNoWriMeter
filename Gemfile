eeb5efedf425d16a27105cb5ad7e5f24f0cc5c71
Raquel Moss <raquelxmoss@gmail.com>
2015-10-21 09:27:12 +1300

    Add some gems, start omniauth setup

--- Gemfile
+++ Gemfile
@@ -19,8 +19,10 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-gem "d3-rails"
-gem "octokit"
+gem 'd3-rails'
+gem 'httparty'
+gem 'devise'
+gem 'omniauth-github'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -33,6 +35,7 @@ gem "octokit"
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'rspec-rails'
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def get_files
+    response = HTTParty.get("#{Rails.application.secrets.github_endpoint}/#{current_user.github_username}/#{current_user.repo_name}/contents")
+    response.each { |file| get_file_contents(file) }
+  end
+
+  def get_file_contents(file)
+    file = current_user.files.find_or_create_by(name: file["name"])
+    contents = HTTParty.get("#{Rails.application.secrets.github_endpoint}/#{current_user.github_username}/#{current_user.repo_name}/contents/#{file["name"]}",
+      headers: {"Accept": "application/vnd.github-blob.raw"})
+    file.update(content: contents)
+  end
+
+  def parse_contents
+  end
 end
