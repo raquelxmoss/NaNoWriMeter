ab657013ce2108a18d5ab17a09af58e78947fc7c
Raquel Moss <raquelxmoss@gmail.com>
2015-10-24 14:01:45 +1300

    Facebook is not github, obviously

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
+  def github
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
