92e4d10e7509b1ba9c567b0b11f018977214c9f1
Raquel Moss <raquelxmoss@gmail.com>
2015-10-24 14:43:42 +1300

    Please please please

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
 	root 'application#index'
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
-  devise_scope :user do
-      get '/users/auth/:provider' => 'users/omniauth_callbacks#github'
-  end
 end
