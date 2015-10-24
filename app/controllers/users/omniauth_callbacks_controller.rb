cf1f30d7e139aa04140f65fdc3d448a7bd290604
Raquel Moss <raquelxmoss@gmail.com>
2015-10-24 22:21:14 +1300

    Add non-omniauth sign in for development

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,4 +4,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     sign_in @user
     redirect_to root_path
   end
+
+  def dev_sign_in
+    redirect_to controller: :application, action: :index unless Rails.env.development?
+    sign_in(User.find_by_email('raquelxmoss@gmail.com'))
+    redirect_to :back
+  end
+
 end
