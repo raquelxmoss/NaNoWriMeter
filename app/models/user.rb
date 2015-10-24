205ad238b0d6078b5ace98da2bfe3edba0286af4
Raquel Moss <raquelxmoss@gmail.com>
2015-10-24 14:24:15 +1300

    Alter from omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:github]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+    where(
+      :github_username => auth[:info][:nickname]
+      ).first_or_create do |user|
+        user.email = auth[:info][:email]
+        user.password = Devise.friendly_token[0,20]
     end
   end
 
