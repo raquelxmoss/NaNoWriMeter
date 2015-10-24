6173f8ad9ebe7082b0e578ce566d0d7d6df1cb81
Raquel Moss <raquelxmoss@gmail.com>
2015-10-24 15:00:26 +1300

    add github username to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020201814) do
+ActiveRecord::Schema.define(version: 20151024015950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151020201814) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "github_username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
