e90cdb54a3a706ef6b1c7de7b5dca07c8b3f9c60
Raquel Moss <raquelxmoss@gmail.com>
2015-10-24 22:10:26 +1300

    add routes for user snippets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 	root 'application#index'
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
+  resources :users do
+  	resources :snippets, only: [:create, :destroy, :index, :show]
+  end
 end
