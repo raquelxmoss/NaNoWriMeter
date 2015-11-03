56f532465d70cf929f19aec8614f6c256b770c44
Raquel Moss <raquelxmoss@gmail.com>
2015-11-03 18:42:12 +1300

    I am not a smart lady.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.sign_in_count > 1
+    if @user.sign_in_count < 1
       sign_in @user
       redirect_to user_settings_path(@user)
     else
