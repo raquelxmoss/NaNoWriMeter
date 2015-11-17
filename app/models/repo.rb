929ea34eae4960ecfa507c2a6140163d96d18fa2
Raquel Moss <raquelxmoss@gmail.com>
2015-11-17 19:56:36 +1300

    Move repo methods to repo model

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,5 +1,54 @@
 class Repo < ActiveRecord::Base
   belongs_to :user
 
+  def update_word_count
+    files = get_files
+    count = files.map {|f| HTTParty.get("https://raw.githubusercontent.com/#{user.github_username}/#{name}/master/#{f["name"]}")
+                           .split(" ")
+                           .length }.reduce(:+)
+    update(word_count: count)
+  end
+
+  def calculate_word_frequency
+    result = Hash.new
+    files = get_files
+    words = files.map {|f| HTTParty.get("https://raw.githubusercontent.com/#{user.github_username}/#{name}/master/#{f["name"]}")
+                           .split(" ").map { |w| w.downcase.strip.gsub(/[^a-z|A-Z]/, "") } }
+                           .flatten
+                           .reject(&:empty?)
+                           .reject {|word| STOP_WORDS.include?(word) }
+    words.uniq.each { |w| result[w] = words.count(w) }
+    result.sort_by {|word, count| count }.reverse
+  end
+
+  def get_files
+    HTTParty.get("https://api.github.com/repos/#{user.github_username}/#{name}/contents")
+                .select {|f| f["name"].downcase != "README.md".downcase }
+  end
+
+  def get_diff
+    snippets = []
+    commits = HTTParty.get(get_url)
+    commits.each do |commit|
+      url = commit["html_url"]
+      commit_message = commit["commit"]["message"]
+      diff = open("#{url}.diff") { |diff_file| diff_file.read }
+        .split("\n")
+        .select { |line| line.start_with?('+') }
+        .reject { |line| line.start_with?('++') }
+        .map { |line| line.slice(1..-1) }
+        .join("\n")
+      snippets <<  { diff: diff, message: commit_message }
+    end
+    snippets
+  end
+
+  def get_url
+    if last_submit?
+      "https://api.github.com/repos/#{user.github_username}/#{name}/commits?since=#{user.last_submit.to_s.gsub(" ", "%20")}"
+    else
+      "https://api.github.com/repos/#{user.github_username}/#{name}/commits"
+    end
+  end
   
 end
